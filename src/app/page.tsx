ce70ba162edc263c13deecb56ad6d73a61949387
Estani02 <estaniolmedo@gmail.com>
2023-07-17 17:29:56 -0300

    create Display3 component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Display1 from '@/components/display1'
 import Display2 from '@/components/display2'
+import Display3 from '@/components/display3'
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[rgb(21,24,28)] text-white">
       <Display1 />
       <Display2 />
+      <Display3 />
     </main>
   )
 }
