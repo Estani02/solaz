a58428ed51d831fad36325afaa62a8b58a3aa38d
Estani02 <estaniolmedo@gmail.com>
2023-07-17 17:30:36 -0300

    added components items

--- src/components/display2/index.tsx
+++ src/components/display2/index.tsx
@@ -4,9 +4,9 @@ import CarouselCompont from '../carousel'
 
 function Display2() {
   return (
-    <div className="relative flex h-[80vh] flex-col items-center overflow-hidden bg-[#202128] bg-gradient-to-b p-2 text-white">
-      <h3 className="mt-2">Beneficios</h3>
-      <div className="container mx-auto my-5 grid grid-cols-1 gap-2 text-center md:grid-cols-4 md:gap-4 xl:px-20">
+    <div className="relative flex h-[80vh] flex-col items-center p-2 text-white">
+      <h3 className="mt-2 text-2xl font-extrabold uppercase">Beneficios</h3>
+      <div className="container mx-auto my-5 grid grid-cols-1 gap-2 text-center  md:gap-4 xl:px-20">
         <CarouselCompont />
       </div>
     </div>
--- src/components/items/IteamCarousel.tsx
+++ src/components/items/IteamCarousel.tsx
@@ -9,14 +9,14 @@ interface PropsItem {
 function IteamCarousel({ name, description, img }: PropsItem) {
   return (
     <div
-      className="relative m-auto h-[50vh] w-full bg-cover bg-center bg-no-repeat text-white"
+      className="relative m-auto h-[50vh] w-full bg-cover bg-center bg-no-repeat text-white md:h-[60vh] "
       style={{
         backgroundImage: `url(${img})`,
       }}
     >
       <div className="absolute inset-0 bg-black opacity-[0.6]" />
-      <div className="relative p-5 text-left">
-        <h2 className=" text-xl font-bold uppercase">{name}</h2>
+      <div className="relative p-5 text-left md:flex md:h-full md:w-full md:flex-col md:items-center md:justify-center md:text-justify">
+        <h2 className="text-xl font-bold uppercase">{name}</h2>
         <p className="text-left text-sm font-medium">{description}</p>
       </div>
     </div>
