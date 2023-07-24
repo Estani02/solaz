723a5562534b0dcb6dd4620940de4c655e677e73
Estani02 <estaniolmedo@gmail.com>
2023-07-24 18:09:27 -0300

    added display4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Display1 from '@/components/display1'
-import Display2 from '@/components/display2'
-import Display3 from '@/components/display3'
+import Display1 from '@/components/displays/display1'
+import Display2 from '@/components/displays/display2'
+import Display3 from '@/components/displays/display3'
+import Display4 from '@/components/displays/display4'
 
 export default function Home() {
   return (
-    <main className="bg-[rgb(21,24,28)] text-white">
+    <main className="bg-[#15181c] text-white">
       <Display1 />
       <Display2 />
       <Display3 />
+      <Display4 />
     </main>
   )
 }
