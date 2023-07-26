76c7219fd28c4964df9af340daa801e16018a498
Estani02 <estaniolmedo@gmail.com>
2023-07-26 17:18:41 -0300

    new domines img

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lirp.cdn-website.com'],
+    // domains: ['lirp.cdn-website.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lirp.cdn-website.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.styku.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'architect.bjc.es',
+      },
+      {
+        protocol: 'https',
+        hostname: 'blogs.funiber.org',
+      },
+    ],
   },
 }
 
