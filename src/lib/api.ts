52b692f854c8023d35a5b65bc70af9fe5cdba573
Estani02 <estaniolmedo@gmail.com>
2023-08-04 14:03:44 -0300

    nodemailer configuration

---
+++ src/lib/api.ts
@@ -0,0 +1,26 @@
+import type { FormValues } from '@/interfaces'
+
+export async function sendFormContact(formData: FormValues) {
+  await fetch('http://localhost:3000/api/contact', {
+    method: 'POST', // Método de solicitud
+    headers: {
+      'Content-Type': 'application/json', // Tipo de contenido que se envía en el cuerpo de la solicitud
+    },
+    body: JSON.stringify(formData), // Convertir el objeto formData a JSON
+  })
+    .then((response) => {
+      // Manejar la respuesta del servidor
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+
+      return response.json() // Leer el cuerpo de la respuesta como JSON
+    })
+    .then((data) => {
+      console.log(data) // Aquí puedes manejar la respuesta del backend
+    })
+    .catch((error) => {
+      console.error('Error:', error) // Manejar errores en caso de que ocurran
+      throw error
+    })
+}
