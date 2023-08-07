84f69a9d269141817f703c80cc686a0531e40955
Estani02 <estaniolmedo@gmail.com>
2023-08-07 16:12:31 -0300

    reverted changes

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,17 +1,17 @@
 import nodemailer from 'nodemailer'
 
-const email = process.env.EMAIL
+const email = process.env.NEXT_PUBLIC_EMAIL
 const pass = process.env.NEXT_PUBLIC_EMAIL_PASS
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'testsolaz@gmail.com',
-    pass: 'wvdtnhmvdacbfple',
+    user: email,
+    pass: pass,
   },
 })
 
 export const mailOptions = {
-  from: 'testsolaz@gmail.com',
-  to: 'testsolaz@gmail.com',
+  from: email,
+  to: email,
 }
